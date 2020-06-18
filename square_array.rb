380506be4d25928a3b6ad6ec457dee5f57640636
miloscomplex <hicallmesutton@gmail.com>
2020-06-18 10:40:48 -0500

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,21 @@
+# def square_array(array)
+#   new_array= []
+#   array.each_with_index do | num, index |
+#     new_array[index] = num * num
+#   end
+#   new_array
+# end
+#
+
 def square_array(array)
-  # your code here
+  array.each_with_index do | num, index |
+    array[index] = num * num
+  end
 end
+
+# in terminal [1,2,3] returns [1,4,9]
+# def square_array(array)
+#   array.collect do | num |
+#     array= num * num
+#   end
+# end
